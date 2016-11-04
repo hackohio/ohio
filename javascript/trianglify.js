360cc96ce437993932515c63042b38d7092e1f53
Kevin Payravi <kevinpayravi@gmail.com>
2016-11-04 16:22:34 -0400

    Adding notice near top on registration status

--- javascript/trianglify.js
+++ javascript/trianglify.js
@@ -3,7 +3,7 @@ var pattern = Trianglify({
 	x_colors: ["#B00126", "#FDF2F3", "#B00126"],
 	y_colors: 'match_x',
 	width: window.innerWidth,
-	height: 1250
+	height: 1370
 });
 var canvas = document.getElementById("headerCanvas");
 pattern.canvas(canvas);
