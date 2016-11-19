7fdd4e393fa2d18474174c6e91598025810a332a
Kevin Payravi <kevinpayravi@gmail.com>
2016-11-19 16:24:21 -0500

    Updating countdown to hacking end

--- javascript/countdown.js
+++ javascript/countdown.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	"use strict";
 
 	// NOTE: this is the UTC time of the event!
-	var hackingStarts = moment("2016-11-19 11:00:00-04:00");
+	var hackingStarts = moment("2016-11-20 11:00:00-04:00");
 	var timeUntilStart = hackingStarts.diff(moment(), 'seconds');
 
 	var clock = $('.countdown').FlipClock(timeUntilStart, {
