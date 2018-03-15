28d7195414d23f9b09a2ebf1bbd5a59285d86332
Caitlyn <caitlyn@Caitlyns-MBP.attlocal.net>
2018-03-14 21:18:01 -0400

    Added workshops, linked attendee guide

--- js/trianglify.js
+++ js/trianglify.js
@@ -23,12 +23,23 @@ var pattern3 = Trianglify({
 	width: window.innerWidth,
 	height: 50
 });
+
+var pattern4 = Trianglify({
+	cell_size: 100,
+	x_colors: ["#60605F", "#2D302D", "#222"],
+	y_colors: 'match_x',
+	width: window.innerWidth,
+	height: 50
+});
 var canvas = document.getElementById("bar1");
 var canvas2 = document.getElementById("bar2");
 var canvas3 = document.getElementById("bar3");
+var canvas4 = document.getElementById("bar4");
+
 pattern.canvas(canvas);
 pattern2.canvas(canvas2);
 pattern3.canvas(canvas3);
+pattern4.canvas(canvas4);
 
 
 // Detect desktop browser resize; re-draw canvas when detected
