43e2f337fe2d120470cd296c67c22793632455f4
Caitlyn <caitlyn@Caitlyns-MacBook-Pro.local>
2018-03-17 12:46:48 -0400

    Added sponsors

--- js/trianglify.js
+++ js/trianglify.js
@@ -31,16 +31,24 @@ var pattern4 = Trianglify({
 	width: window.innerWidth,
 	height: 50
 });
+var pattern5 = Trianglify({
+	cell_size: 100,
+	x_colors: ["#222", "#2D302D", "#60605F"],
+	y_colors: 'match_x',
+	width: window.innerWidth,
+	height: 50
+});
 var canvas = document.getElementById("bar1");
 var canvas2 = document.getElementById("bar2");
 var canvas3 = document.getElementById("bar3");
 var canvas4 = document.getElementById("bar4");
+var canvas5 = document.getElementById("bar5");
 
 pattern.canvas(canvas);
 pattern2.canvas(canvas2);
 pattern3.canvas(canvas3);
 pattern4.canvas(canvas4);
-
+pattern5.canvas(canvas5);
 
 // Detect desktop browser resize; re-draw canvas when detected
 jQuery(function($){
