f8607bb8f09253140d9d63723e17b4f7f0c3c174
kelly <kelly.wu.98@gmail.com>
2018-11-28 09:21:10 -0500

    adding make

---
+++ make/2019/basic-landing/slides.js
@@ -0,0 +1,9 @@
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
