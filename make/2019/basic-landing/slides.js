c92b9ae8cc5aec97faa66be4b5bf39a5c94e2c41
kelly <kelly.wu.98@gmail.com>
2018-12-02 18:04:30 -0500

    removing live

--- make/2019/basic-landing/slides.js
+++
@@ -1,9 +0,0 @@
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
-
-function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
-}
