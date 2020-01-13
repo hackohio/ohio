8d652a8170793b99ea27f95b85307a694ffed43d
alvin-law <alvinl.0701@gmail.com>
2020-01-13 15:26:26 -0500

    Update make sponsors, clock, initial pages

--- make/2020/script/clock.js
+++ make/2020/script/clock.js
@@ -1,7 +1,8 @@
-var hackingStarts = "2019-02-02T12:00:00-05:00";
+var hackingStarts = "2020-02-29T10:00:00-05:00";
+initializeClock('countdown', hackingStarts);
 
-function getTimeRemaining(endtime){
-  var t = Date.parse(endtime) - Date.parse(new Date());
+function getTimeRemaining(endTime){
+  var t = Date.parse(endTime) - Date.parse(new Date());
   var seconds = Math.floor( (t/1000) % 60 );
   var minutes = Math.floor( (t/1000/60) % 60 );
   var hours = Math.floor( (t/(1000*60*60)) % 24 );
@@ -15,26 +16,29 @@ function getTimeRemaining(endtime){
   };
 }
 
-function initializeClock(id, endtime){
-  var clock = document.getElementById(id);
-	var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+function initializeClock(id, endTime){
+    var clock = document.getElementById(id);
+    clock.innerHTML = '<div><span class="days time"></span><div class="time-label">days</div></div>'
+        + '<div><span class="hours time"></span><div class="time-label">hours</div></div>'
+        + '<div><span class="minutes time"></span><div class="time-label">minutes</div></div>'
+        + '<div><span class="seconds time"></span><div class="time-label">seconds</div></div>';
 
-	function updateClock(){
-  	var t = getTimeRemaining(endtime);
-		daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  	if(t.total<=0){
-    	clearInterval(timeinterval);
-		}
-  }
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
 
-	updateClock(); // run function once at first to avoid delay
-	var timeinterval = setInterval(updateClock,1000);
+    function updateClock(){
+        var t = getTimeRemaining(endTime);
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+        if(t.total<=0){
+            clearInterval(timeInterval);
+        }
+    }
+
+    updateClock(); // run function once at first to avoid delay
+    var timeInterval = setInterval(updateClock,1000);
 }
-
-initializeClock('countdown', hackingStarts);
