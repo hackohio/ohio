a361efe1ff5cfdd1fe181fe1ba486edb2fb35bab
thomasdw33 <dawit.4@osu.edu>
2020-02-09 21:37:04 -0500

    Removed live

--- make/2020/live/script/nav.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-  var x = document.getElementById("nav-links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
