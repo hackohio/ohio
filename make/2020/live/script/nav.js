3b65b5f09d0488eeb9d92356afd9270c1be7d196
thomasdw33 <56744083+thomasdw33@users.noreply.github.com>
2020-02-25 17:30:15 -0500

    Add base live site for Make2020 (#314)

---
+++ make/2020/live/script/nav.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+  var x = document.getElementById("nav-links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
